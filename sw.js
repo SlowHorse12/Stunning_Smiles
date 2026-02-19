742bff9aa6b49bdadd4c5790bd881f169ad5f7bb
SlowHorse12 <holisticdentistrysantarosa@gmail.com>
2026-02-19 15:37:05 -0800

    Initial commit - Punjabi Voice Translator web app

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'punjabi-voice-v1';
+const urlsToCache = [
+  '/google-tts-voice.html',
+  '/voice-to-voice.html',
+  '/enhanced-voice-to-voice.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
